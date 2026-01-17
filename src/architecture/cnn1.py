f7802e48972b41d086e91d4c829e3f00de950d0b
Deepa <deepaksutradhar415@gmail.com>
2026-01-17 13:24:27 +0530

    cnn1

---
+++ src/architecture/cnn1.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+
+class CNNArchitecture(nn.Module):
+    def __init__(self, input_shape=(128, 128, 32)):
+        super().__init__()
+
+        self.conv_blocks = nn.Sequential(
+            self.conv_block(1, 8),
+            self.conv_block(8, 16),
+            self.conv_block(16, 32),
+            self.conv_block(32, 64),
+            self.conv_block(64, 128),
+        )
+
+        D, H, W = input_shape
+        for i in range(5):
+            D, H, W = D // 2, H // 2, W // 2
+        self.flat_dim = 128 * D * H * W
+
+        self.final_layer = nn.Sequential(
+            nn.Linear(self.flat_dim, 64),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+        )
+
+    
+    def conv_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.MaxPool3d(kernel_size=2, stride=2),
+        )
+    
+    def forward(self, x):
+        x = self.conv_blocks(x)
+        x = torch.flatten(x, start_dim=1) #(batch_size, 128, 1, 4, 4) -> (batch_size, 2048)
+        x = self.final_layer(x) 
+        return x 
--- src/preprocess/rescale.py
+++ src/preprocess/rescale.py
@@ -1,14 +1,16 @@
 from scipy.ndimage import zoom
 
+MID_PART = 32
+
 def rescaled_data(data):
     D = data.shape[2]
 
-    start = (D - 32) // 2
-    end = start + 32
+    start = (D - MID_PART) // 2
+    end = start + MID_PART
 
     middle_parts = data[:, :, start:end]
 
-    target_size = (128, 128, 32)
+    target_size = (128, 128, MID_PART)
     zoom_factors = (target_size[0]/data.shape[0], target_size[1]/data.shape[1], 1)
 
     resized_data = zoom(middle_parts, zoom_factors, order=1)
