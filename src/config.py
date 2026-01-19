60fa1a494170c1c7b0529530d235e83590f53634
Deepa <deepaksutradhar415@gmail.com>
2026-01-19 18:45:52 +0530

    bug-fixes

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ import torch
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 8
 LEARNING_RATE = 1e-3
-N_EPOCHS = 10
+N_EPOCHS = 5
 NUM_WORKERS = 2
 SAVE_MODEL = False
 LOAD_MODEL = False
